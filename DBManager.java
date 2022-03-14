bd47a38c358ce34c1f6a57c36a89ad8ab4ed069b
Cheta Ezenwoye <88289210+naechebango@users.noreply.github.com>
2022-03-14 14:31:16 +0000

    Add files via upload

--- DBManager.java
+++ DBManager.java
@@ -12,7 +12,7 @@ public class DBManager {
             String user = "root";
 
 //            input your workbench password
-            String password = "your-password";
+            String password = "Ezenwoye123!";
 
 //            input url to database
             String url = "jdbc:mysql://localhost:3306/comp5590";
@@ -34,12 +34,52 @@ public class DBManager {
             assert connection != null;
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from doctors");
+            String result;
             while ((resultSet.next())) {
-                System.out.println(resultSet.getString("DocID") + " - " + resultSet.getString("Name") + " - " + resultSet.getString("Phone") + " - " +  resultSet.getString("Speciality") + " - " +  resultSet.getString("Gender")  );
+                result = resultSet.getString("DocID") + " - " + resultSet.getString("Name") + " - " + resultSet.getString("Phone") + " - " +  resultSet.getString("Speciality") + " - " +  resultSet.getString("Gender");
+                System.out.println(result);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getAllPat(){
+        StringBuilder result = new StringBuilder();
+        try {
+            Connection connection = getConnection();
+            assert connection != null;
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from patients");
+            String details;
+            while ((resultSet.next())) {
+                details = resultSet.getString("PatientID") + " - " + resultSet.getString("Name") + " - " + resultSet.getString("Number") + " - " +  resultSet.getString("PrefDoc") + " - " +  resultSet.getString("Gender");
+                System.out.println(details);
+                result.append(details);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+    public String getAllBookings(){
+        StringBuilder result = new StringBuilder();
+        try {
+            Connection connection = getConnection();
+            assert connection != null;
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from bookings");
+            String details;
+            while ((resultSet.next())) {
+                details = resultSet.getString("Date") + " - " + resultSet.getString("Time") + " - " + resultSet.getString("Doctor") + " - " +  resultSet.getString("Patient") + " - " + resultSet.getString("Topic") + "\n";
+//                System.out.println(details);
+                result.append(details);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result.toString();
     }
 
 //    insert a new row in doctor table
@@ -55,6 +95,30 @@ public class DBManager {
         getAllDoc();
     }
 
+    public void insertPat(String patId, String name, int number, String prefDoc, String gender) throws Exception{
+        try {
+            Connection connection = getConnection();
+            assert connection != null;
+            PreparedStatement insert = connection.prepareStatement("INSERT INTO `comp5590`.`patients` (`PatientID`, `Name`, `Number`, `PrefDoc`, `Gender`) VALUES ('" + patId + "', '" + name + "', '" + number + "', '" + prefDoc + "', '" + gender + "')");
+            insert.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        getAllPat();
+    }
+
+    public void insertBooking(String date, String time, String doctor, String patient, String topic) throws Exception{
+        try {
+            Connection connection = getConnection();
+            assert connection != null;
+            PreparedStatement insert = connection.prepareStatement("INSERT INTO `comp5590`.`bookings` (`Date`, `Time`, `Doctor`, `Patient`, `Topic`) VALUES ('" + date + "', '" + time + "', '" + doctor + "', '" + patient + "', '" + topic + "')");
+            insert.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        getAllBookings();
+    }
+
     public static void main(String[] args) throws Exception {
 //        Scanner myObj = new Scanner(System.in);
 //        String docId = myObj.nextLine();
@@ -66,8 +130,8 @@ public class DBManager {
 //        String gender = myObj2.nextLine();
 
 //        getConnection();
+        getAllPat();
         getAllDoc();
-
 
     }
 }
